2586dc409cdf97d5c989e3406f1a3b3ac4fb3f75
Jeremías <jeremiias122002@gmail.com>
2023-10-29 20:11:35 +0100

    setting controller functions

--- controllers/web_api-controller.js
+++ controllers/web_api-controller.js
@@ -2,6 +2,36 @@
 const db = require('../models');
 const Web_api_backend = db.web_api_model;
 
+exports.create = (req, res) => {
+
+    // validando petición
+    if (!req.body.title) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    // creando modelo
+    const webApiBackend = new Web_api_backend({
+        type: req.body.type,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    });
+
+    // guardándolo en la BBDD
+    webApiBackend   
+        .save(webApiBackend)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Algún error ocurrió mientras se creaba el modelo."
+            });
+        });
+};
+
 exports.findAll = (req, res) => {
 
     Web_api_backend.find();
